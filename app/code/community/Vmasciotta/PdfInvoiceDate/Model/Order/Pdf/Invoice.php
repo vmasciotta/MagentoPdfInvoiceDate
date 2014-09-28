62234718384e56a4ca3b05f2fc6c42151c3a5366
Valerio Masciotta <info@regento.it>
2014-09-28 15:23:43 +0200

    First import

---
+++ app/code/community/Vmasciotta/PdfInvoiceDate/Model/Order/Pdf/Invoice.php
@@ -0,0 +1,68 @@
+<?php
+class Vmasciotta_PdfInvoiceDate_Model_Order_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Invoice
+{
+    /**
+     * Return PDF document
+     *
+     * @param  array $invoices
+     * @var    Mage_Sales_Model_Order_Invoice $invoice
+     * @return Zend_Pdf
+     */
+    public function getPdf($invoices = array())
+    {
+        $this->_beforeGetPdf();
+        $this->_initRenderer('invoice');
+
+        $pdf = new Zend_Pdf();
+        $this->_setPdf($pdf);
+        $style = new Zend_Pdf_Style();
+        $this->_setFontBold($style, 10);
+
+        foreach ($invoices as $invoice) {
+            /**  @var Mage_Sales_Model_Order_Invoice $invoice */
+
+            if ($invoice->getStoreId()) {
+                Mage::app()->getLocale()->emulate($invoice->getStoreId());
+                Mage::app()->setCurrentStore($invoice->getStoreId());
+            }
+            $page  = $this->newPage();
+            $order = $invoice->getOrder();
+            /* Add image */
+            $this->insertLogo($page, $invoice->getStore());
+            /* Add address */
+            $this->insertAddress($page, $invoice->getStore());
+            /* Add head */
+            $this->insertOrder(
+                $page,
+                $order,
+                Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID, $order->getStoreId())
+            );
+            /* Add document text and number */
+            $this->insertDocumentNumber(
+                $page,
+                Mage::helper('sales')->__('Invoice # ') . $invoice->getIncrementId() . " - " . Mage::helper('core')->formatDate(
+                    $invoice->getCreatedAt(), 'medium', false
+                )
+            );
+
+            /* Add table */
+            $this->_drawHeader($page);
+            /* Add body */
+            foreach ($invoice->getAllItems() as $item){
+                if ($item->getOrderItem()->getParentItem()) {
+                    continue;
+                }
+                /* Draw item */
+                $this->_drawItem($item, $page, $order);
+                $page = end($pdf->pages);
+            }
+            /* Add totals */
+            $this->insertTotals($page, $invoice);
+            if ($invoice->getStoreId()) {
+                Mage::app()->getLocale()->revert();
+            }
+        }
+        $this->_afterGetPdf();
+        return $pdf;
+    }
+}
